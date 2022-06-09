51c26739fcda4cc039dc9664ca959a01442b62ee
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2022-06-09 09:34:48 +0200

    Apply code formatting (`mix format`)

--- router/lib/router/dispatcher.ex
+++ router/lib/router/dispatcher.ex
@@ -1,13 +1,13 @@
 defmodule Router.Dispatcher do
   use Plug.Router
 
-  if Mix.env == :dev do
+  if Mix.env() == :dev do
     use Plug.Debugger
   end
 
-  plug :match
-  plug :dispatch
+  plug(:match)
+  plug(:dispatch)
 
-  forward "/alice", to: Alice.Plug
-  forward "/bob", to: Bob.Plug
+  forward("/alice", to: Alice.Plug)
+  forward("/bob", to: Bob.Plug)
 end
